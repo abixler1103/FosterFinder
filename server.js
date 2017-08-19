fce63f6e0c8aa00ac3b9f4469dd35b14762a102b
Tictac0723 <taylorclark0723@gmail.com>
2017-08-19 13:27:24 -0500

    add api code

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+var db = require("./models");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+app.use(express.static("./public"));
+
+app.use(methodOverride("_method"));
+
+require("./controllers/dogs_controller.js")(app);
+require("./controllers/foster_controller.js")(app);
+
+db.sequelize.sync().then(function(){
+	app.listen(PORT, function(){
+		console.log("App listening on PORT " + PORT);
+	});
+});
